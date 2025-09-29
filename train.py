b03a6a9c46e4e5bbda850dd5020550871c319c80
Raiya Minhas <raiya.minhas@gmail.com>
2025-09-29 01:10:12 -0400

    train.py mods

--- train.py
+++ train.py
@@ -1,2 +1,13 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
+
+env = gym.make("CartPole-v1")
+
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/")
+
+timesteps = 100_000
+model.learn(total_timesteps=timesteps)
+
+model.save("models/ppo_cartpole")
+
+env.close()
